7e3c5867c4e00170ce543a2782f085f3b69e60a6
chengscott <60510scott@gmail.com>
2018-03-03 23:27:34 +0800

    Change to precedent increment

--- matrix.c
+++ matrix.c
@@ -4,7 +4,7 @@
 void matrix(int row, int col, int array[row][col])
 {
     int i, j;
-    for (i = 0; i < row; i++)
-        for (j = 0; j < col; j++)
-            array[i][j]++;
+    for (i = 0; i < row; ++i)
+        for (j = 0; j < col; ++j)
+            ++array[i][j];
 }
